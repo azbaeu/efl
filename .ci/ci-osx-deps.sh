cf41600b0e486931512a6a3537bbe47ba6f6b13a
Marcel Hollerbach <mail@marcel-hollerbach.de>
2018-08-01 00:00:56 +0200

    and this one ?

--- .ci/ci-osx-deps.sh
+++ .ci/ci-osx-deps.sh
@@ -3,4 +3,4 @@
 brew update
 brew unlink python
 brew install gettext check bullet dbus fontconfig freetype fribidi gst-plugins-good gstreamer luajit openssl webp libsndfile glib libspectre libraw librsvg poppler lz4 pulseaudio ccache
-export CFLAGS="-I/usr/local/opt/openssl/include $CFLAGS" LDFLAGS="-L/usr/local/opt/openssl/lib $LDFLAGS"
+export CFLAGS="-I/usr/local/opt/openssl/include $CFLAGS" LDFLAGS="-L/usr/local/opt/openssl/lib $LDFLAGS" PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
